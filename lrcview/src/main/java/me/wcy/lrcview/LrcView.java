30ab2f9c1566e9a46e3ece0d1951bad9adfd0413
JELLY BEAN <46275808+jellybean13@users.noreply.github.com>
2019-02-19 10:22:25 +0800

    Add missing license (Part 3)

--- lrcview/src/main/java/me/wcy/lrcview/LrcView.java
+++ lrcview/src/main/java/me/wcy/lrcview/LrcView.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright (C) 2017 wangchenyan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
 package me.wcy.lrcview;
 
 import android.animation.ValueAnimator;
@@ -347,8 +361,9 @@ public class LrcView extends View {
         // 无歌词文件
         if (!hasLrc()) {
             mLrcPaint.setColor(mCurrentTextColor);
-            @SuppressLint("DrawAllocation") StaticLayout staticLayout = new StaticLayout(mDefaultLabel, mLrcPaint, (int) getLrcWidth(), Layout.Alignment.ALIGN_CENTER, 1f, 0f,
-                    false);
+            @SuppressLint("DrawAllocation")
+            StaticLayout staticLayout = new StaticLayout(mDefaultLabel, mLrcPaint,
+                    (int) getLrcWidth(), Layout.Alignment.ALIGN_CENTER, 1f, 0f, false);
             drawText(canvas, staticLayout, centerY);
             return;
         }
